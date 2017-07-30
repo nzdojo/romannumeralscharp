c7817566ae192a61eccd67a778f894904885e053
Nick Zdunic <n_zdunic@yahoo.com.au>
2017-07-30 16:59:52 -0400

    Initial commit of dotnet core project and failing test

---
+++ RomanNumerals.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace NumberSystems
+{
+    public class RomanNumerals
+    {
+        public bool FromArabic(int arabicNumber) 
+        {
+            throw new NotImplementedException("Please create a test first");
+        } 
+    }
+}
---
+++ RomanNumeralsTests.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using NumberSystems;
+
+namespace NumberSystems
+{
+    public class RomanNumeralsTests
+    {
+        [Fact]
+        public void FromArabicZeroConvertsToNothing()
+        {
+            Assert.True(false, "We don't have anything yet");
+        }
+    }
+}
